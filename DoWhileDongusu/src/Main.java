c7285b2fad54e3cdedf58ece9ea075591ed4082a
Mehmet Nuri ÖZTÜRK <mnozturk@outlook.com.tr>
2022-01-30 14:28:39 +0300

    Do while döngüsü eklendi

---
+++ DoWhileDongusu/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * do{
+         * işlemler}
+         * while(kosul)
+         */
+
+//        int n = 10;
+//
+//        do {
+//            System.out.println(n);
+//            n--;
+//        }while (0<=n);
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int toplam = 0;
+
+
+    }
+}
