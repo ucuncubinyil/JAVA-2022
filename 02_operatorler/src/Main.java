2df125490f98a50067b36e035cbf596753fefdb9
Mehmet Nuri ÖZTÜRK <mnozturk@outlook.com.tr>
2022-01-30 14:27:20 +0300

    Operatorler dersi eklendi

---
+++ 02_operatorler/src/Main.java
@@ -0,0 +1,87 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        int x = 30;
+//        int y = 3;
+//
+//        System.out.println("X ile Y nin toplamı : " + (x + y));
+//        System.out.println("X ile Y nin farkı : " + (y - x));
+//        System.out.println("X ile Y nin çarpımı : " + (y * x));
+//        System.out.println("X ile Y nin bölümü : " + (x / y));
+
+//        int x = 56;
+//        int y =  3;
+//
+//        System.out.println("X 'in Y'ye bölümünden kalan :  " + (x%y));
+
+//        int sayi = 56;
+//        int sayi2 = 5;
+//        System.out.println("Arttırmadan öncekki sayi degeri = "+sayi);
+//        System.out.println("Azaltmadan öncekki sayi2 degeri = "+sayi2);
+//        sayi++;
+//        sayi2--;
+//        System.out.println("Arttırmadan sonraki sayi degeri = "+sayi);
+//        System.out.println("Azaltmadan sonraki sayi degeri2 = "+sayi2);
+
+//        int sayi = 5;
+//        System.out.println("İşlem yapılmadan önceki değeri " + sayi);
+//
+//        sayi += 5;
+//        System.out.println("Arttırıp eşitledikten sonraki değeri :" + sayi);
+//
+//        sayi -= 1;
+//        System.out.println("Eksiltip eşitledikten sonraki değeri :" + sayi);
+//
+//        sayi *= 2;
+//        System.out.println("Çarpıp eşitledikten sonraki değeri :" + sayi);
+//
+//        sayi /= 2;
+//        System.out.println("Bölüp eşitledikten sonraki değeri :" + sayi);
+//
+//        sayi %= 2;
+//        System.out.println("Mod alıp eşitledikten sonraki değeri :" + sayi);
+
+//        int a = 10;
+//        int b = 5;
+//
+//        /*
+//        * > Büyüktür
+//        * < Küçüktür
+//        * == eşittir
+//        * != eşit değildir
+//        * <= Küçük veya eşit
+//        * >= Büyük veya eşit
+//        *
+//        * */
+//
+//        System.out.println("A b ye eşit mi ? : " + (a == b));
+//
+//        System.out.println("A B den küçük veya eşit mi ? : " + (a <= b));
+//
+//        System.out.println("A B den büyük veya B ye eşit mi? : " + (a >= b));
+//
+//        System.out.println("A B ye eşit değil mi? : " + (a != b));
+//
+//        System.out.println("A B den büyük mü : " + (a > b));
+//
+//        System.out.println("A B den küçük mü : " + (a < b));
+
+
+        /**
+         * Matıksal Operatörler
+         *
+         * && (Ve) Operatörü
+         * || (Veya ) Operatörü
+         */
+
+        int a =  15;
+        int b = 20;
+
+        System.out.println( (a<b) && (b>=20) );
+
+        System.out.println( (a<b) || (a>16) );
+
+
+    }
+}
